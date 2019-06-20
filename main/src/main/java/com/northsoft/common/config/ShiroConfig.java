21f235ad8d36dac6cdbcfed5028e892486636bea
quchao <670980756>
2019-06-20 10:28:16 +0800

    版本：1.0.0 修改内容：数据源配置，添加微信登录退出 涉及业务：登录、登出 涉及BUG：无

--- main/src/main/java/com/northsoft/common/config/ShiroConfig.java
+++ main/src/main/java/com/northsoft/common/config/ShiroConfig.java
@@ -81,6 +81,7 @@ public class ShiroConfig {
         filterMap.put("/sys/login", "anon");
         filterMap.put("/favicon.ico", "anon");
         filterMap.put("/captcha.jpg", "anon");
+        filterMap.put("/wx/**", "anon");
         filterMap.put("/**", "authc");
         shiroFilter.setFilterChainDefinitionMap(filterMap);
 
